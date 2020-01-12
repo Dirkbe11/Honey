cb0fb9087cb8a421c9844096f863d29af5386069
Dirk Bell <jamesdirkbell@gmail.com>
2020-01-12 17:39:49 -0600

    Add voice command for clear song queue

--- honey/audiocontroller.py
+++ honey/audiocontroller.py
@@ -96,6 +96,9 @@ class AudioController(commands.Cog):
         if(command.startswith('help')):
             asyncio.run_coroutine_threadsafe(self._help_voice_command(), loop)
 
+        if(command.startswith('clear')):
+             asyncio.run_coroutine_threadsafe(self.music.clear(self.voice_ctx), loop)
+             
         if(command.startswith('EE1')):
             print("suit")
             asyncio.run_coroutine_threadsafe(self.music.play(self.voice_ctx, "https://www.youtube.com/watch?v=x2qRDMHbXaM"), loop)
